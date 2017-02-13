1f979b859682d54767516f6e7bc633e83567cbe9
DragonVirus <limonster.li@gmail.com>
2017-02-14 00:09:40 +0630

    init repo : Clone php-endpoint-bootstrap

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Hexcores\MongoLite\Connection as MongoConnection;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $connection = MongoConnection::instance();
+
+        $this->app->instance('connection', $connection);
+    }
+}
